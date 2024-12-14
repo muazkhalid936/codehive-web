6bf669be2a1841024d00579ab2312f4ded00670a
Muaz Khalid <itzmajo786@gmail.com>
2024-12-14 11:38:33 +0500

    aa

--- src/app/page.js
+++ src/app/page.js
@@ -23,9 +23,9 @@ export default function Home() {
 
         <OurIndustries />
         <Test />
-        <ContactForm />
+        {/* <ContactForm /> */}
 
-        {/* <Connect /> */}
+        <Connect />
       </div>
     </>
   );
