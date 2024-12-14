e73b86d4ca6e1d8c5b4846f36b895daa0671dc5e
Muaz Khalid <itzmajo786@gmail.com>
2024-12-14 14:58:59 +0500

    Working on animation

--- src/app/page.js
+++ src/app/page.js
@@ -5,23 +5,26 @@ import ContactForm from "../components/Contact";
 import Stack from "../components/Stack";
 import Test from "../components/Test";
 import WhySection from "../components/WhySection";
-import AnimatedHeading from "../components/AnimatedHeading";
 import OurIndustries from "../components/OurIndustries";
-import GoalHeading from "../components/GoalHeading";
 import Connect from "../components/Connect";
+import TextReveal from "../components/GoalHeading/TextReveal";
 export default function Home() {
   return (
     <>
       <div className="bg-[#000B17]">
         <MainHero />
-        <GoalHeading />
-
+        {/* <GoalHeading /> */}
+        <TextReveal text="Why You Choose Us?" />
         <WhySection />
+        <TextReveal text="Our Services" />
         <Stack />
-        <AnimatedHeading />
+        {/* <AnimatedHeading /> */}
+        <TextReveal text="Our Process" />
         <OurProcess />
 
+        <TextReveal text="Our Industries" />
         <OurIndustries />
+        <TextReveal text="Our Recent Works" />
         <Test />
         {/* <ContactForm /> */}
 
